d64033be7eb21f10dbdbc3b09f86259040ff700b
Sean Zheng <SeanZheng21@gmail.com>
2020-07-28 16:02:41 +0800

    Initial Commit

---
+++ CaloriePalTests/CaloriePalTests.swift
@@ -0,0 +1,34 @@
+//
+//  CaloriePalTests.swift
+//  CaloriePalTests
+//
+//  Created by Carlistle ZHENG on 7/28/20.
+//  Copyright © 2020 Carlistle ZHENG. All rights reserved.
+//
+
+import XCTest
+@testable import CaloriePal
+
+class CaloriePalTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
